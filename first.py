4249454d3e3d38bb981b00dee5afa4de36c613c2
Blake Hooper <blake.hooper@gmail.com>
2017-05-24 06:58:40 +1000

    initial commit

---
+++ first.py
@@ -0,0 +1,9 @@
+print("hello world", 5)
+
+i = 0
+total = 0
+while i <= 20:
+    total = total + i
+    i = i + 1
+
+print("The total = ", total)
---
+++ titanic.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import pandas as pd
+
+
+train = pd.read_csv("./resources/titanic-data/train.csv")
+
