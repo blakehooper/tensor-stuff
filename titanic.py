4cbbc26137a81023782216b460f5c9a903d0ccd2
Blake Hooper <blake.hooper@underwriteme.co.uk>
2017-06-14 18:35:03 +1000

    Created DNN for titanic

--- titanic.py
+++ titanic.py
@@ -1,58 +1,82 @@
-import tensorflow as tf
 import numpy as np
-import csv
+import pandas as pd
+import tensorflow as tf
 
-file_name = "resources/titanic-data/train.csv"
-with open(file_name, 'r') as f:
-    print(f.readline())
+def buildSuccess(x):
+  result = np.ones((2))
+  result[int(x)] = 0
+  return result
 
-train_data = np.genfromtxt(file_name, delimiter=",", skip_header=1, dtype=str)
+def readData(fileName):
+  train_data = pd.read_csv(fileName)
 
-print(train_data.shape)
+  train_data["Age"] = train_data["Age"].fillna(train_data["Age"].median())
 
-# for row in train_data:
-#     print(row[6])
+  train_data.loc[train_data["Sex"] == "male", "Sex"] = 0
+  train_data.loc[train_data["Sex"] == "female", "Sex"] = 1
 
-expected = np.reshape(np.array(train_data[:, 1], dtype=np.int8), (891, 1))
-#
-input_sex = np.array([1 if i == 'female' else 0 for i in train_data[:, 5]], dtype=np.int)
+  train_data["Embarked"] = train_data["Embarked"].fillna("S")
 
-input_age = np.array([28 if i == '' else i for i in train_data[:, 6]], dtype=np.float)
+  train_data.loc[train_data["Embarked"] == "S", "Embarked"] = 0
+  train_data.loc[train_data["Embarked"] == "C", "Embarked"] = 1
+  train_data.loc[train_data["Embarked"] == "Q", "Embarked"] = 2
 
-input = np.transpose(np.array([input_sex, input_age]))
+  predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
-print(expected.shape)
-print(input.shape)
+  return train_data[predictors], np.array([buildSuccess(i) for i in train_data["Survived"]])
 
-# Build graph
+train_data, train_expected = readData("./resources/titanic-data/train.csv")
+#test_data, test_expected = readData("./resources/titanic-data/test.csv")
 
-x = tf.placeholder(tf.float32, shape=[891, 2])
-W = tf.Variable(tf.zeros([2, 1]))
-b = tf.Variable(tf.zeros([1]))
+def accuracy(produced, provided):
+  return (100.0 * np.sum(np.argmax(produced, 1) == np.argmax(provided, 1))
+          / produced.shape[0])
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+graph = tf.Graph()
+with graph.as_default():
 
-y_ = tf.placeholder(tf.float32, [891, 1])
+  #test_dataset = tf.constant(test_data)
 
-#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+  ## Input layer
+  # an input size 891, 7
+  train_dataset = tf.placeholder(tf.float32, shape=(891, 7))
+  # an input size 891, 2
+  expected_placeholder = tf.placeholder(tf.float32, shape=(891, 2))
 
-sess = tf.InteractiveSession()
+  ## Hidden layer 1
+  hidden_nodes = 1024
+  # Trainable size 7 X 1024
+  hidden_weights = tf.Variable(tf.truncated_normal([7, hidden_nodes]))
+  # Trainable size 1 X 1024
+  hidden_biases = tf.Variable(tf.zeros([hidden_nodes]))
 
-tf.global_variables_initializer().run()
+  hidden_layer = tf.nn.relu(tf.matmul(train_dataset, hidden_weights) + hidden_biases)
 
-# for _ in range(1000):
-#   batch_xs, batch_ys = mnist.train.next_batch(100)
-sess.run(train_step, feed_dict={x: input, y_: expected})
+  # Output layer
+  # Trainable size 1024 x 1
+  weights = tf.Variable(tf.truncated_normal([1024, 2]))
+  # Trainable size 1 X 1
+  biases = tf.Variable(tf.zeros([2]))
 
-# test model
-correct_prediction = tf.equal(y, y_)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
+  logits = tf.matmul(hidden_layer, weights) + biases
 
+  #Write logits as probabilites
+  train_prediction = tf.nn.softmax(logits)
 
+  # Define loss and optimizer
+  # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=train_expected))
+  beta = 0.01
+  loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=train_expected)
+  loss = tf.reduce_mean(loss + beta * tf.nn.l2_loss(hidden_weights) + beta * tf.nn.l2_loss(weights))
+  optimizer = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
 
+  # test1 = tf.nn.relu(tf.matmul(test_dataset, hidden_weights) + hidden_biases)
+  # test_prediction = tf.nn.softmax(tf.matmul(test1, weights) + biases)
+
+with tf.Session(graph=graph) as session:
+  tf.global_variables_initializer().run()
+  for x in range(20):
+    feed_dict = {train_dataset: train_data, expected_placeholder: train_expected }
+    _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+    print("train batch accuracy: %.1f%%" % accuracy(predictions, train_expected))
 
